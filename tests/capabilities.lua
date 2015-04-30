8a7dc1f24def8f418346c7b324582616161c3de2
Mário Kašuba <mario.kasuba@it-academy.sk>
2015-04-30 18:38:38 +0200

    Added sample code for capabilities

---
+++ tests/capabilities.lua
@@ -0,0 +1,9 @@
+﻿local zmq = require 'luazmq'
+
+print('A list of supported capabilities:')
+
+for _, capability in ipairs {
+	'ipc', 'pgm', 'tipc', 'norm', 'curve', 'gssapi',
+} do
+	print(("%s : %s"):format(capability, zmq.has(capability) and "true" or "false"))
+end
